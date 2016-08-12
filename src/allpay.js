3fd55235ae734540e9c99000581f226848a50f22
yanchen <yan771012@gmail.com>
2016-08-12 16:29:36 +0800

    npm initial

---
+++ src/allpay.js
@@ -0,0 +1,19 @@
+
+
+const MODE = {
+  production: 'https://newlogistics.allpay.com.tw',
+  stage: 'https://newlogistics-stage.allpay.com.tw'
+};
+
+const API = {
+  CREATE: '/Express/Create'
+};
+
+class Allpay {
+  constructor() {
+
+  }
+}
+
+
+module.exports = Allpay;
